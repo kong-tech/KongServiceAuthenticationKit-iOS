f3b4dc77a83fba84494a0f38cb555e49229b577c
kongljh <kongljh@kong-tech.com>
2020-09-10 13:18:57 +0900

    Initial Commit

---
+++ KongServiceAuthenticationKit/KongServiceAuthenticationKit.h
@@ -0,0 +1,19 @@
+//
+//  KongServiceAuthenticationKit.h
+//  KongServiceAuthenticationKit
+//
+//  Created by leejinhae on 2020/09/10.
+//  Copyright © 2020 KONGTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KongServiceAuthenticationKit.
+FOUNDATION_EXPORT double KongServiceAuthenticationKitVersionNumber;
+
+//! Project version string for KongServiceAuthenticationKit.
+FOUNDATION_EXPORT const unsigned char KongServiceAuthenticationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KongServiceAuthenticationKit/PublicHeader.h>
+
+
